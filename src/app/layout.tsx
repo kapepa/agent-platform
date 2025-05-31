4523524ac97ad989215ac1a93dff48cc1ddc24d1
kapepa <kapepayx@gmail.com>
2025-05-31 13:54:52 +0300

    trpc setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { TRPCReactProvider } from "@/trpc/client";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,12 +29,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <TRPCReactProvider>
+      <html lang="en">
+        <body
+          className={`${inter.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </TRPCReactProvider>
   );
 }
--- src/app/modules/home/ui/views/home-views.tsx
+++ src/app/modules/home/ui/views/home-views.tsx
@@ -2,34 +2,20 @@
 
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { useTRPC } from "@/trpc/client";
 import { Routers } from "@/types/routers";
+import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 
 function HomeView() {
-  const router = useRouter();
-  const { data: session } = authClient.useSession();
-
-  if (!session) {
-    return (
-      <p>Loading...</p>
-    )
-  }
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "Karma" }))
 
   return (
     <div
       className="flex flex-col p-4 gap-y-4"
     >
-      <p>Logged is as {session?.user.name}</p>
-      <Button
-        type="button"
-        onClick={() => authClient.signOut({
-          fetchOptions: {
-            onSuccess: () => router.push(Routers.SignIn)
-          }
-        })}
-      >
-        Sign out
-      </Button>
+
     </div>
   );
 }
